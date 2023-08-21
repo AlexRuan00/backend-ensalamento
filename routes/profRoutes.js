0fde3b03dcfb047dfeb31dc68b249a6e6596cfc6
FABRÍCIO DOS SANTOS <fabricio_santos5@estudante.sesisenai.org.br>
2023-08-21 20:01:07 -0300

    feat: add a GET endpoint for list professor

--- routes/profRoutes.js
+++ routes/profRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/profControllers');
 
 router
-.post('/', controller.registerProfessor);
+.post('/', controller.registerProfessor)
+.get('/', controller.listProfessor)
 
 module.exports = router;
