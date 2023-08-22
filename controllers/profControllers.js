ce283223a18abf801c654209c76b412da2e178e8
Felipezkg <felipezkg@gmail.com>
2023-08-21 21:01:38 -0300

    feat: add a PUT endpoint for updating professor

--- controllers/profControllers.js
+++ controllers/profControllers.js
@@ -6,10 +6,27 @@ const database = require('../models/database');
      const values = [nome]
         database.query(query, values).then(
          ()=> {
-             res.status(201).send({ mensagem: 'Professor successfully registered.' });
+             res.status(201).send({ message: 'Professor successfully registered.' });
          },
          (erro) => {
              res.status(500).send({ erro: erro });
          }
      )
  }
+
+ exports.updateProfessor = (req, res) => {
+    const oldName = req.params.nome;
+    const query = 'UPDATE professores SET nome_professor=$2 WHERE nome_professor=$1';
+    const values = [oldName, req.body.nome];
+
+    database.query(query, values).then(
+        () => {
+            res.status(201).send({
+                message: 'Professor successfully updated!'
+            });
+        },
+        (erro) => {
+            res.status(500).send({ erro: erro });
+        }
+    );
+};
--- routes/profRoutes.js
+++ routes/profRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/profControllers');
 
 router
-.post('/', controller.registerProfessor);
+.post('/', controller.registerProfessor)
+.put('/:nome', controller.updateProfessor);
 
 module.exports = router;
