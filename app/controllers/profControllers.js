b206d4806e6dbb47c959edcefdbe6c5ba034734f
ALEX RUAN RIBEIRO <alex_rr@estudante.sc.senai.br>
2023-09-11 21:03:06 -0300

    feat: Creating the query to fill in the professores_materias table

--- app/controllers/profControllers.js
+++ app/controllers/profControllers.js
@@ -1,12 +1,35 @@
 const database = require('../../config/database');
 
+const registerProfDiscipline = (idProf, idsDiscipline) => {
+    for (let i = 0; i < idsDiscipline.length; i++) {
+        const id = idsDiscipline[i];
+        const query = 'INSERT INTO  professores_materias (id_professor, id_materia) VALUES($1, $2);'
+        const values = [idProf, id];
+        database.query(query, values).then(
+            () => {
+                console.log("professor_materia successfully registered.")
+            },
+            (erro) => {
+                console.log(erro)
+            }
+        )
+    }
+}
+
 exports.registerProfessor = (req, res) => {
-    const { nome, dias, quantidadeDias } = req.body;
-    console.log(nome, dias, quantidadeDias);
+    const { nome, dias, quantidadeDias, idMateria } = req.body;
     const query = 'INSERT INTO professores (nome_professor, dias_disponiveis, quantidade_dias) VALUES($1, $2, $3);'
+    const queryLastProf = 'SELECT * FROM professores ORDER BY id_professor DESC LIMIT 1;'
     const values = [nome, dias, quantidadeDias]
     database.query(query, values).then(
         () => {
+            database.query(queryLastProf).then(
+                (result) => {
+                    const idProfessor = result.rows[0].id_professor
+                    registerProfDiscipline(idProfessor, idMateria)
+                }
+            )
+
             res.status(201).send({ message: 'Professor successfully registered.' });
         },
         (erro) => {
@@ -44,7 +67,7 @@ exports.listProfessor = (req, res) => {
 
 
 exports.updateProfessor = (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const { nome, dias, quantidadeDias } = req.body;
     console.log(nome, dias, quantidadeDias, id)
     const query = 'UPDATE professores SET nome_professor=$1, dias_disponiveis=$2, quantidade_dias=$3 WHERE id_professor=$4;';
@@ -60,3 +83,5 @@ exports.updateProfessor = (req, res) => {
         }
     );
 };
+
+
